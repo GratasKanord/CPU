4e500d0c5e7392c1f07fa157968e56d0ec739b5c
Gratas Kanord <gratas.kanord8@gmail.com>
2025-11-15 13:43:37 +0100

    rv64ui-p-bgeu (34) test passed

--- cpu_internal/imem/imem.v
+++ cpu_internal/imem/imem.v
@@ -14,7 +14,7 @@ reg [31:0] imem [0:MEM_SIZE - 1];
 integer i;
 initial begin
     //$readmemh("./ASMcode/tests/instructions/auipc/test.hex", imem);
-    $readmemh("./compl_tests/rv64ui-p-bltu.hex", imem);
+    $readmemh("./compl_tests/rv64ui-p-bgeu.hex", imem);
     
     
 end
